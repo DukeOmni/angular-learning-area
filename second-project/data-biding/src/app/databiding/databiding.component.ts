9eb725fb0e36d77595f5a75f8722b4070c5375cc
Caio Riserio <caioscript@gmail.com>
2021-07-01 19:40:55 -0300

    Input Properties learning

--- second-project/data-biding/src/app/databiding/databiding.component.ts
+++ second-project/data-biding/src/app/databiding/databiding.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatabidingComponent implements OnInit {
   url:String ='http://google.com'
+  name:String='Gaijin-san'
   constructor() { }
 
   ngOnInit(): void {
---
+++ second-project/data-biding/src/app/input-properties/input-properties.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-properties',
+  templateUrl: './input-properties.component.html',
+  styleUrls: ['./input-properties.component.css']
+})
+export class InputPropertiesComponent implements OnInit {
+
+  @Input()foreigner='';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
