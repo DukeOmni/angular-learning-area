8555f290126eb987200ba5c14c1ee3c951d11c0a
Caio Riserio <caioscript@gmail.com>
2021-07-21 12:20:25 -0300

    Messing with ng-content

--- third-project/src/app/app.module.ts
+++ third-project/src/app/app.module.ts
@@ -5,12 +5,15 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 import { DiretivaAtributoComponent } from './diretiva-atributo/diretiva-atributo.component';
 import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
 import { ElvisOperatorComponent } from './elvis-operator/elvis-operator.component';
+import { NgContentComponent } from './ng-content/ng-content.component';
 
 
 @NgModule({
@@ -20,11 +23,13 @@ import { ElvisOperatorComponent } from './elvis-operator/elvis-operator.componen
     DiretivaNgswitchComponent,
     DiretivaAtributoComponent,
     DiretivaNgstyleComponent,
-    ElvisOperatorComponent
+    ElvisOperatorComponent,
+    NgContentComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatExpansionModule,
     MatButtonModule,
     MatTabsModule,
     MatIconModule
